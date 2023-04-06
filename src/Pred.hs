d441976ff85d8346093078926ccd9dc707ef9910
Davidc465 <julian.carrillo.465@mi.unc.edu.ar>
2023-04-06 18:59:49 -0300

    Pred.hs 'cannot construct the infinite type' error corrected

--- src/Pred.hs
+++ src/Pred.hs
@@ -2,22 +2,22 @@ module Pred (
   Pred,
   cambiar, anyDib, allDib, orP, andP
 ) where
+import Dibujo
 
 type Pred a = a -> Bool
 
 -- Dado un predicado sobre básicas, cambiar todas las que satisfacen
 -- el predicado por la figura básica indicada por el segundo argumento.
-cambiar :: Pred a -> (a -> Dibujo a) -> Dibujo a -> Dibujo a
-cambiar pre fun dibu = mapDib fun dibu
 
-cambiarPb :: Pred a -> (a -> Dibujo a) -> Dibujo a -> Dibujo a
-cambiarPb pre fun dibu = foldDib (applyfun lmnt) id id id 
-                                                  (apilar f1 f2 (cambiarPb pre fun d1) (cambiarPb pre fun d2))
-                                                  (juntar f1 f2 (cambiarPb pre fun d1) (cambiarPb pre fun d2))
-                                                  (encimar (cambiarPb pre fun d1) (cambiarPb pre fun d2))
+cambiar :: Pred a -> (a -> Dibujo a) -> Dibujo a -> Dibujo a
+cambiar pre fun dibu = foldDib (\lmnt -> applyfun lmnt) id id id 
+                                                  (\f1 f2 d1 d2 -> apilar f1 f2 (cambiar pre fun d1) (cambiar pre fun d2))
+                                                  (\f1 f2 d1 d2 -> juntar f1 f2 (cambiar pre fun d1) (cambiar pre fun d2))
+                                                  (\d1 d2 -> encimar (cambiar pre fun d1) (cambiar pre fun d2))
+                                                  dibu
                                   where applyfun lmnt = case pre lmnt of
-                                                          true = fun lmnt
-                                                          false= id
+                                                          True -> fun lmnt
+                                                          False ->figura lmnt
 
 -- Alguna básica satisface el predicado.
 anyDib = undefined
